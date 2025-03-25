0a28acbc47603f1112b792822df456f0691157a0
Guilherme Bartasson <guilherme.bartasson@simpleagro.com.br>
2025-03-25 16:22:25 -0300

    Feat: Button "Nossa Localização"

--- src/pages/Find-us.tsx
+++ src/pages/Find-us.tsx
@@ -1,5 +1,17 @@
+import '../styles/findus.scss'
 export default function FindUs() {
     return (
-        <> Find Us </>
+        <>
+            <div>
+                <div className="a">Onde nos encontrar ?</div>
+            </div>
+            <div className='b'>
+                <button>
+                    <img src="/pino.png" className='img'/>
+                    Nossa localização
+                </button>
+            </div>
+        </>
     );
 }
+
