cf97e129af7d65a8c466edd44e2616694778a5b2
Guilherme Bartasson <guilherme.bartasson@simpleagro.com.br>
2025-03-26 17:33:13 -0300

    Feat: Finishing styling the page and adding a redirect button to Google Maps

--- src/pages/Find-us.tsx
+++ src/pages/Find-us.tsx
@@ -1,17 +1,26 @@
 import '../styles/findus.scss'
+
 export default function FindUs() {
+    const Redirect = (): void => {
+        window.open("https://maps.app.goo.gl/MiSiCGs1ik6JfZPy7");
+      };
+    
     return (
         <>
-            <div>
-                <div className="a">Onde nos encontrar ?</div>
+            <div className='d'>
+                <div className="a">
+                    Onde nos encontrar ?
+                </div>
+                 <div className='b'>
+                    <button className='bt' onClick={Redirect}>
+                        <img src="/pino.png" className='img'/>
+                        Nossa localização
+                    </button>
+                </div>
             </div>
-            <div className='b'>
-                <button>
-                    <img src="/pino.png" className='img'/>
-                    Nossa localização
-                </button>
+            <div className='c'>
+                 <img src="/clinica.png" className='img2' />
             </div>
         </>
     );
 }
-
